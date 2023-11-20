ce65c131f6245f02eb4873992ebf3fe2d2b1f5f9
neeraj0422 <neeraj13031998@gmail.com>
2023-11-21 00:11:57 +0530

    month fix

--- src/components/taskmanager/RecordTable.jsx
+++ src/components/taskmanager/RecordTable.jsx
@@ -6,7 +6,6 @@ import { Doughnut } from 'react-chartjs-2';
 import PieChart from './PieChart';
 
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
